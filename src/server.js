1a5bcd2fcd1c7308573bdd12cfa0d254ed5a49bc
Lucio-jose <luciocanganjo2@gmail.com>
2022-07-04 14:44:11 +0100

    add operations

--- src/server.js
+++ src/server.js
@@ -7,18 +7,25 @@ app.use(express.json());
 
 const contas=[];
 
-app.get("/", (Request, Response)=>{
-    return Response.json({message:"Ola mundo, eu estou funcionando"})
-})
-
-app.post("/user", (Request, Response)=>{
-    const { cpf, name}=Request.body
-
+function verificarConta(Request, Response, next){
+    const { cpf}=Request.body
     const conta= contas.find(conta=>conta.cpf === cpf);
 
     if(conta){
         return Response.status(404).json({message:"Conta existente"})
     }
+
+    return next()
+}
+
+app.get("/", (Request, Response)=>{
+    return Response.json({message:"E aí parceiro! Voce acessou a FinanceAPI"})
+})
+
+app.post("/user", verificarConta,(Request, Response)=>{
+    const { cpf, name}=Request.body
+
+  
 
     contas.push({
         cpf,
@@ -34,18 +41,79 @@ app.get("/users", (Request, Response)=>{
     return Response.json(contas)
 });
 
-app.get("/extrato/:cpf", (Request, Response)=>{
+function verificarExtrato(Request, Response, next){
     const {cpf}= Request.params;
-
     const conta= contas.find(conta=>conta.cpf === cpf);
 
     if(!conta){
         return Response.status(404).json({message:"Conta não encontrada"})
     }
 
-    return Response.status(200).json(conta.statement)
+    Request.conta = conta
+
+    return next()
+}
+
+function getBalanco(statement){
+   const balance = statement.reduce((acomulador, operacao)=>{
+        if(operacao.type==='credit'){
+            return acomulador + operacao.valor
+        }else{
+            return acomulador - operacao.valor
+        }
+    },0);
+
+    return balance;
+}
+
+app.get("/extrato/:cpf", verificarExtrato, (Request, Response)=>{
+    const {conta}= Request
+
+return Response.status(200).json(conta.statement)
 });
 
+app.get("/statement", verificarExtrato, (Request, Response)=>{
+
+})
+
+app.post("/deposit/:cpf", verificarExtrato, (Request, Response)=>{
+const {description, valor}=Request.body;
+
+const {conta}=Request;
+
+const statementOperation={
+    description, 
+    valor,
+    creatDate: new Date(),
+    type:'credit'
+}
+
+conta.statement.push(statementOperation);
+
+return Response.status(201).json({message:"Deposito bem sucedido", conta})
+})
+
+
+app.post("/debito/:cpf",verificarExtrato,(Request, Response)=>{
+const {valor}=Request.body;
+const {conta}=Request;
+ 
+const balance = getBalanco(conta.statement)
+
+if(balance<valor){
+    return Response.status(400).json({message:"Operação inválida"})
+}
+
+const statementOperation={
+    description, 
+    valor,
+    creatDate: new Date(),
+    type:'debit'
+}
+
+conta.statement.push(statementOperation);
+return Response.status(201).send()
+})
 
 app.listen(3033, ()=>{
     console.log("Servidor Rodando")
